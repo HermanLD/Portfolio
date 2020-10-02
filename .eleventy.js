93ed779623e0865af00fbeac3eda870fd94b1d9b
Herman Dardon <hdardon91@gmail.com>
2020-10-01 21:54:56 -0700

    Update to 11ty

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+const htmlCss = require("./_data/htmlCssWorks");
+const jsWork = require("./_data/jsWorks");
+const vueWork = require("./_data/vueWorks");
+
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy("./css");
+
+  eleventyConfig.addCollection("featured", function() {
+    let arr1 = htmlCss.filter((el) => el.isFeat === "true");
+    let arr2 = jsWork.filter((el) => el.isFeat === "true");
+    let arr3 = vueWork.filter((el) => el.isFeat === "true");
+    return [...arr1,...arr2,...arr3];
+  });
+};
