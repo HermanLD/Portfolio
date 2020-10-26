a3a1ad7f75edb85451e29dc22da93291395c176e
Herman Dardon <hdardon91@gmail.com>
2020-10-26 10:29:56 -0700

    update data files isFeat

--- .eleventy.js
+++ .eleventy.js
@@ -7,9 +7,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./css");
 
   eleventyConfig.addCollection("featured", function() {
-    let arr1 = htmlCss.filter((el) => el.isFeat === "true");
-    let arr2 = jsWork.filter((el) => el.isFeat === "true");
-    let arr3 = vueWork.filter((el) => el.isFeat === "true");
+    let arr1 = htmlCss.filter((el) => el.isFeat == true);
+    let arr2 = jsWork.filter((el) => el.isFeat == true);
+    let arr3 = vueWork.filter((el) => el.isFeat == true);
     return [...arr1,...arr2,...arr3];
   });
 };
